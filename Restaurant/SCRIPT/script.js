34d8535b8ca0d45a8db4179261ccb13a793d0063
Yangesi <yangesi123@gmail.com>
2021-08-18 18:29:11 -0600

    commit

--- Restaurant/SCRIPT/script.js
+++ Restaurant/SCRIPT/script.js
@@ -1,4 +1,31 @@
-const addToShoppingCartButtons = document.querySelectorAll('#btncomprar');
-addToShoppingCartButtons.forEach((addToCartButton) => {
-  addToCartButton.addEventListener('click', () => console.log(`click`));
-});
+const ClickButton = document.querySelectorAll('.button')
+let carrito = []
+
+ClickButton.forEach(btn => { btn.addEventListener('click', addCarItem)
+})
+
+function addCarItem(e){
+  const button = e.target
+  const item = button.closest('.product')
+  const itemTitle = item.querySelector('.title').textContent;
+  const itemPrice = item.querySelector('.price').textContent;
+  const itemImg = item.querySelector('.imgp').src;
+  
+  const newItem ={
+    title: itemTitle,
+    price: itemPrice,
+    img: itemImg,
+    cantidad: 1
+  }
+
+  addItemCarrito(newItem)
+}
+
+function addItemCarrito(newItem){
+  carrito.push(newItem)
+  renderCarrito()
+}
+
+function renderCarrito(){
+  console.log(carrito)
+}
